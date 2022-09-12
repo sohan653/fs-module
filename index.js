2b17b19030e331bdf2771fb6cf5a9b2194695911
sohan653 <sohanurl653@gmail.com>
2022-09-13 02:18:23 +0600

    readFile addd

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var http=require('http');
 var fs= require('fs');
+
+var server=http.createServer((req,res)=>{
+
+    if(req.url="/"){
+        fs.readFile("home.html",(error,data)=>{
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data)
+        })
+    }
+})
+server.listen(5000,()=>{
+    console.log("Server started on port 5000")
+})
